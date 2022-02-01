710fb26ab99d485c2e9b69e0610ac180426ad76b
Saravanan2366 <letsmailsaravanan@gmail.com>
2022-02-02 03:05:36 +0530

    R's update code

--- src/test/java/org/testing/Sample.java
+++ src/test/java/org/testing/Sample.java
@@ -3,6 +3,8 @@ package org.testing;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Test1");
+		System.out.println("test6");
+		System.out.println("test7");
 	}
 
 }
